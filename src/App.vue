856899e262cee1e97eab5561c8bf6954b8ec999e
Simon Wikstrand <simon@sunstate.se>
2017-09-14 21:02:37 +0200

    Added some game functions

--- src/App.vue
+++ src/App.vue
@@ -24,8 +24,14 @@
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
-  margin-top: 60px;
+  background: url('./assets/bakgrund_hela.svg') bottom left no-repeat;
+  background-size: 80%;
 }
+
+  * {
+    margin: 0;
+    padding: 0;
+  }
 </style>
--- src/components/Game.vue
+++ src/components/Game.vue
@@ -4,19 +4,24 @@
 
 <script>
   import * as PIXI from 'pixi.js'
+
   export default {
     data () {
       return {
         renderer: null,
         stage: null,
-        logo: null,
+        whacka: null,
         x: 0,
-        keyState: {}
+        y: 400,
+        groundLevel: 400,
+        keyState: {},
+        JumpState: false,
+        pSpeedY: 0
       }
     },
     name: 'hello',
     mounted () {
-      this.renderer = PIXI.autoDetectRenderer(512, 512, {
+      this.renderer = PIXI.autoDetectRenderer(window.innerWidth * 0.8, 700, {
         resolution: 1,
         transparent: 1
       })
@@ -24,24 +29,61 @@
       this.$el.appendChild(this.renderer.view)
       this.stage = new PIXI.Container()
 
-      PIXI.loader.add('logo', require('../assets/logo.png')).load(this.setup)
-
-      window.addEventListener('keydown', () => {
-        this.x += 10
-      })
+      PIXI.loader.add('whacka', require('../assets/with-backpack.svg')).load(this.setup)
     },
     methods: {
+      keyListener () {
+        const keyState = {}
+
+        window.addEventListener('keydown', (e) => {
+          keyState[e.keyCode || e.which] = true
+        }, true)
+
+        window.addEventListener('keyup', (e) => {
+          keyState[e.keyCode || e.which] = false
+        }, true)
+
+        setInterval(() => {
+          if (keyState[37] || keyState[65]) {
+            this.x -= 3
+          }
+
+          if (keyState[39] || keyState[68]) {
+            this.x += 3
+          }
+
+          if ((keyState[87] || keyState[38]) && this.y === this.groundLevel) {
+            this.pSpeedY = 15
+          }
+        }, 10)
+      },
       setup () {
-        this.logo = new PIXI.Sprite(
-          PIXI.loader.resources['logo'].texture
+        this.whacka = new PIXI.Sprite(
+          PIXI.loader.resources['whacka'].texture
         )
 
-        this.stage.addChild(this.logo)
+        this.whacka.scale.set(0.3)
+        // this.whacka.pivot
+
+        this.stage.addChild(this.whacka)
         this.animationLoop()
+        this.keyListener()
       },
       animationLoop () {
+        this.y = this.y - this.pSpeedY
+
+        if (this.y < this.groundLevel) {
+          // this.whacka.rotation += window.Math.PI / 15
+          this.pSpeedY -= 1
+        } else {
+          this.pSpeedY = 0
+          this.y = this.groundLevel
+        }
+
         requestAnimationFrame(this.animationLoop)
-        this.logo.x = this.x
+        this.whacka.x = this.x
+        this.whacka.y = this.y
+
         this.renderer.render(this.stage)
       }
     }
